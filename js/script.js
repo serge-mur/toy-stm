5ade779693bd189151b0f4bda112e69b75311cf0
serge <serge-mur@yandex.ru>
2022-11-01 17:00:41 +0300

    working..

--- js/script.js
+++ js/script.js
@@ -12,3 +12,24 @@ document.addEventListener('DOMContentLoaded', function(event) {
     mediaChange(mediaQueryMobile)
 
 })
+
+let scrollpos = window.scrollY
+const header = document.querySelector(".top-navbar")
+const scrollChange = 130
+window.addEventListener('scroll', function() { 
+    scrollpos = window.scrollY;
+    if (scrollpos >= scrollChange) {
+        header.classList.add("navbar-bg")
+    }
+    else {
+        header.classList.remove("navbar-bg")
+    }
+})
+
+// const bsOffcanvas = new bootstrap.Offcanvas('#offcanvasMenu')
+// const mobMenuItems = document.querySelectorAll(".side-navbar .nav-link")
+// mobMenuItems.forEach((el) => {
+//     el.addEventListener('click', function() { 
+//         bsOffcanvas.hide()
+//     })
+// });
